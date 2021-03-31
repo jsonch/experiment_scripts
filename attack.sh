3699bf6301ca19a8c1c93b7607f59d5cb3050d35
Joon Kim <joonk@princeton.edu>
2021-03-31 16:06:53 -0400

    update drone attack script

--- attack.sh
+++ attack.sh
@@ -1,5 +1,5 @@
 export TIME=1; 
-for PORT in `seq 5105 5205`; 
-  do  
-    (iperf3 -u -b0 -P2 -c10.64.5.131 -p $PORT -t $TIME) 
-  done
+for PORT in `seq 5201 5203`; 
+do  
+    sudo iperf3 -u -b 0 -P 2 -c 10.64.5.131 -p $PORT -t $TIME &
+done
