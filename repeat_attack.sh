bf2686371c8b9fdee859dc2e3952bbf4461f745b
Joon Kim <hyojoonk@princeton.edu>
2021-04-02 21:20:34 -0400

    add repeat attacks

---
+++ repeat_attack.sh
@@ -0,0 +1,11 @@
+export TIME=$1; 
+export REPEAT=$2;
+export SLEEP=$3;
+for I in `seq 1 $REPEAT`; 
+do
+    for PORT in `seq 5201 5205`; 
+    do  
+        echo "iperf3 -u -b 0 -P 2 -c 10.64.5.131 -p $PORT -t $TIME &"
+    done
+    sleep $SLEEP
+done
