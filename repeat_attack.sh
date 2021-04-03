dd94232f5bc895e6aec9b194a79638a5dde2d7f8
Joon Kim <hyojoonk@princeton.edu>
2021-04-02 21:22:44 -0400

    fix repeat attack

--- repeat_attack.sh
+++ repeat_attack.sh
@@ -5,7 +5,7 @@ for I in `seq 1 $REPEAT`;
 do
     for PORT in `seq 5201 5205`; 
     do  
-        echo "iperf3 -u -b 0 -P 2 -c 10.64.5.131 -p $PORT -t $TIME &"
+        iperf3 -u -b 0 -P 2 -c 10.64.5.131 -p $PORT -t $TIME &
     done
     sleep $SLEEP
 done
